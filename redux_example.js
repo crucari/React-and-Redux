257d6e40ba6104ea0c9267856dbd105a357e64cf
Carina Cruz <lecruz@bu.edu>
2018-09-10 23:35:26 +0000

    notes added to updating the state

--- redux_example.js
+++ redux_example.js
@@ -11,6 +11,16 @@ Function myreducer(state = initState, action){
 If (action.type == ‘ADD_TODO’) {
      Return {
         Todos: [... state.todos, action.todo]]
+
+//updating the state.
+//This return represents the new state of the store.
+//This does not update the state directly.
+//We want to be non-destructive.
+//The new array is spread
+//so we get individual elements out of the state,
+//and then we want to add the new element
+//which in this case is action.to do.
+//The new todo is now added to the todos property//
 }
 }
 }
